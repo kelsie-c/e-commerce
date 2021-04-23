1dda68c7d1384c635ec4eeec484c99b6949447b8
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-04-23 11:21:48 -0400

    completed tag-routes and server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,6 +31,11 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  /* req.body should look like this:
+  {
+    "category_name":"new category"
+  }
+  */
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
