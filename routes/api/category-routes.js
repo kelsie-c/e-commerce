2ee1eceb876f1eba3b334a432669cdd6a11315f4
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-04-23 10:45:10 -0400

    completed category-routes and product-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,7 +42,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update({ category_name: req.body }, { where: { id: req.params.id }});
+    const categoryData = await Category.update(req.body, { where: { id: req.params.id }});
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
